79821836fc419877e940c9f33b121991461960db
ayohizato <ayoshizato.ca@gmail.com>
2022-01-03 19:15:21 -0800

    Second commit step70

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,24 +6,66 @@ import Playlist from '../Playlist/Playlist';
 
 
 class App extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    searchResults: [{name: 'Tchopata', artist: 'artist1', album:'album1', id: 1},
-    {name: 'name2', artist: 'artist2', album:'album2', id: 2},
-    {name: 'name3', artist: 'artist3', album:'album3', id: 3}]
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{name: 'Tchopata', artist: 'artist1', album:'album1', id: 1},
+      {name: 'name2', artist: 'artist2', album:'album2', id: 2},
+      {name: 'name3', artist: 'artist3', album:'album3', id: 3}],
+      playlistName: 'My Playlist',
+      playlistTracks: [{name: 'name4', artist: 'artist4', album:'album4', id: 4}]
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
-}
+  addTrack(track) {
+      let tracks = this.state.playlistTracks;
+      if(tracks.find(savedTrack => savedTrack.id === track.id)) {
+        return;
+      } else {
+        tracks.push(track);
+        this.setState({playlistTracks: tracks})
+    }
+  }
+
+  removeTrack(track) {
+      let tracks = this.state.playlistTracks;
+      tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+      this.setState({playlistTracks: tracks})
+  }
+
+  updatePlaylistName(name) {
+    this.setState({playlistName: name})
+
+  }
+
+  savePlaylist() {
+    //alert('correct');
+    let trackUris = this.state.playlistTracks.map(track => track.uri);
+  }
+
+  search(term) {
+    console.log(term);
+  }
 
   render() {
     return (
       <div>
       <h1>Mi<span className="highlight">yuki's</span>Jam</h1>
       <div className="App">
-        <SearchBar />
+        <SearchBar onSearch={this.search}/>
         <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults}/>
-          <Playlist />
+          <SearchResults searchResults={this.state.searchResults} 
+            onAdd={this.addTrack} />
+          <Playlist playlistName={this.state.playlistName} 
+            playlistTracks={this.state.playlistTracks}
+            onRemove={this.removeTrack} 
+            onNameChange={this.updatePlaylistName}
+            onSave={this.savePlaylist}
+          />
         </div>
       </div>
     </div>
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+            this.search = this.search.bind(this);
+    }
+    
+    search() {
+        this.props.onSearch(this.props.term);
+    }
+
     render() {
         return (<div className="SearchBar">
                 <input placeholder="Enter A Song, Album, or Artist" />
